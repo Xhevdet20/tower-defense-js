8c0afd18a535aec998f7f47faed9459e0c96237c
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-04-29 10:02:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 900;
+canvas.height = 600;
+
+// global variables
+const cellSize = 100;
+const cellGap = 3;
+const gameGrid = [];
+
+// Game Board
+
+// Projectiles
+
+// Defenders
+
+// Enemies
+
+// resources
+
+// Utilities
