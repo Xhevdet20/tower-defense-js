f11c2fbebbe855de702570b65812f897ecbde612
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-04-30 17:00:52 +0200

    enemies

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = 900;
 canvas.height = 600;
+let gameOver = false;
 
 // global variables
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
-
+const defenders = [];
+let numberOfResources = 300;
+const enemies = [];
+const enemyPositions = [];
+let enemiesInterval = 600;
+let frame =0;
 
 // Mouse
 const mouse = {
-  x: undefined,
-  y: undefined,
+  x: 10,
+  y: 10,
   width: 0.1,
   height: 0.1
 }
@@ -43,8 +49,11 @@ class Cell {
   }
 
   draw(){
-    ctx.strokeStyle = 'black';
+    if(mouse.x && mouse.y &&  collision(this, mouse)){
+      ctx.strokeStyle = 'black';
     ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
+    
   }
 }
 
@@ -56,7 +65,6 @@ function createGrid(){
   }
 }
 createGrid();
-
 function handleGameGrid(){
   for(let i =0; i< gameGrid.length; i++){
     gameGrid[i].draw();
@@ -64,19 +72,112 @@ function handleGameGrid(){
 }
 
 // Projectiles
-
 // Defenders
+class Defender {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+    this.shooting = false;
+    this.health = 100;
+    this.projectiles = [];
+    this.timer = 0;
+  }
+  draw(){
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = 'gold';
+    ctx.font = '30px Arial';
+    ctx.fillText(Math.floor(this.health), this.x+15, this.y+30)
+  }
+}
+
+canvas.addEventListener('click', function(){
+  const gridPositionX = mouse.x - (mouse.x % cellSize);
+  const gridPositionY = mouse.y - (mouse.y % cellSize);
+  if(gridPositionY < cellSize) return;
+  for(let i= 0; i< defenders.length; i++){
+    if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY) return;
+  }
+  let defenderCost = 100;
+  if(numberOfResources >= defenderCost){
+    defenders.push(new Defender(gridPositionX, gridPositionY));
+    numberOfResources -= defenderCost;
+  }
+});
+
+function handleDefenders(){
+  for(let i = 0; i< defenders.length; i++){
+    defenders[i].draw();
+  }
+}
 
 // Enemies
+class Enemy {
+  constructor(verticalPosition){
+    this.x = canvas.width;
+    this.y = verticalPosition;
+    this.width = cellSize;
+    this.height = cellSize;
+    this.speed = Math.random() * 0.2 + 0.4;
+    this.movement = this.speed;
+    this.health = 100;
+    this.maxHealth = this.health;
+  }
+  update(){
+    this.x -= this.movement;
+  }
+
+  draw(){
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = 'black';
+    ctx.font = '30px Arial';
+    ctx.fillText(Math.floor(this.health), this.x+15, this.y+30);
+  }
+}
+
+function handleEnemies(){
+  for(let i= 0; i< enemies.length; i++){
+    enemies[i].update();
+    enemies[i].draw();
+    if(enemies[i].x < 0){
+      gameOver = true;
+    }
+  }
+
+  if (frame % enemiesInterval === 0){
+    let verticalPosition = Math.floor(Math.random() * 5 +1) * cellSize;
+    enemies.push(new Enemy(verticalPosition));
+    enemyPositions.push(verticalPosition);
+    if(enemiesInterval > 120) enemiesInterval -=50;
+  }
+}
 
 // resources
 
 // Utilities
+function handleGameStatus(){
+  ctx.fillStyle = 'gold';
+  ctx.font = '30px Arial';
+  ctx.fillText('Resources: ' + numberOfResources,20,55);
+  if(gameOver){
+    ctx.fillStyle = 'black';
+    ctx.font = '60px Arial';
+    ctx.fillText('GAME OVER', 135, 330);
+  }
+}
 function animate(){
+  ctx.clearRect(0,0,canvas.width, canvas.height);
   ctx.fillStyle = 'blue';
   ctx.fillRect(0,0,controlsBar.width, controlsBar.height);
-  handleGameGrid()
-  requestAnimationFrame(animate);
+  handleGameGrid();
+  handleDefenders();
+  handleEnemies()
+  handleGameStatus();
+  frame++;
+  if(!gameOver) requestAnimationFrame(animate);
 };
 
 animate();
@@ -87,6 +188,6 @@ function collision(first, second){
         first.y > second.y + second.height ||
         first.y + first.height < second.y)
       ) {
-        
-      }
-}
+        return true;
+      };
+};
