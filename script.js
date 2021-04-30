d9e9e78ed239d6702ab2dbbea3702e9adfbec2cd
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-04-30 11:05:05 +0200

    grid creation

--- script.js
+++ script.js
@@ -8,7 +8,60 @@ const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
 
+
+// Mouse
+const mouse = {
+  x: undefined,
+  y: undefined,
+  width: 0.1,
+  height: 0.1
+}
+
+let canvasPosition =canvas.getBoundingClientRect();
+canvas.addEventListener('mousemove', function(e){
+  mouse.x = e.x - canvasPosition.left;
+  mouse.y = e.y - canvasPosition.top;
+});
+
+canvas.addEventListener('mouseleave', function(){
+  mouse.x = undefined;
+  mouse.y = undefined;
+});
+
 // Game Board
+const controlsBar = {
+  width: canvas.width,
+  height: cellSize
+}
+
+class Cell {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+  }
+
+  draw(){
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function createGrid(){
+  for(let y= cellSize; y < canvas.height; y+=cellSize){
+    for (let x= 0; x < canvas.width;  x+=cellSize){
+      gameGrid.push(new Cell(x,y))
+    }
+  }
+}
+createGrid();
+
+function handleGameGrid(){
+  for(let i =0; i< gameGrid.length; i++){
+    gameGrid[i].draw();
+  }
+}
 
 // Projectiles
 
@@ -19,3 +72,21 @@ const gameGrid = [];
 // resources
 
 // Utilities
+function animate(){
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(0,0,controlsBar.width, controlsBar.height);
+  handleGameGrid()
+  requestAnimationFrame(animate);
+};
+
+animate();
+
+function collision(first, second){
+  if (!(first.x > second.x +second.width ||
+        first.x + first.width < second.x ||
+        first.y > second.y + second.height ||
+        first.y + first.height < second.y)
+      ) {
+        
+      }
+}
